a9406a6d1a20a51e10c9d5c2faff31358cab736e
Mark Schifferli <mark@fathom.info>
2023-09-28 22:16:31 -0400

    build out the link parser

--- README.md
+++ README.md
@@ -2,18 +2,9 @@
 
 Sometimes you can get your data by downloading a zipped up file. And sometimes, you need to visit every single page on a website and copy it. In the latter case, we call this a scraper, and this code base shows how to make a simple one. 
 
-## parse the document for links
+## handle all the variations of link types. 
 
-To make a useful scraper, you need to find the links in the document, and save them to direct your scraper to visit them too. In this step, we introduce the BeautifulSoup library for traversing html. We use that in the new method we create to look for all the anchor tags `<a>` in a page and get the links from them. We don't go and fetch the next page yet. Instead, let's take a second to look at the links we are getting. 
-
-
-If you run this, you'll see a lot of links look like `/wiki/Appa` or `/wiki/Toph_Beifong`. That's not a url that will work by itself: instead, it's relative to the page it's being called from. We'll have to handle those relative urls for our scraper to work. 
-
-Also, there's a link to `http://www.amazon.com/dp/B000FZETI4`. We don't want to fetch outside our site, and we definitely don't want to start scraping the entire amazon site–it'll go on forever. So if it's not a relative url, we'll want to make sure that the beginning of the url starts with our target site. 
-
-There is also a link to `#`. That's just a different location on the same page: we can skip anything that starts with `#`. 
-
-We'll handle all of those cases next. 
+In addition to relative and absolute links, some links start with `//` instead of `http://` or `https://`. There can be more complexity around relative links as well: are they from a url that ends with '.html' or not? Here the `` method is built out to handle these cases. We take advantage of python's built in urllib library to parse the URL into different parts. We can use those to compare the incoming link to our target, and to build out an absolute link that we can pass into the download method.  
 
 
 ## next
--- scraper.py
+++ scraper.py
@@ -2,9 +2,25 @@
 
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
 
 ''' this is the website we will scrape '''
 URL = "https://avatar.fandom.com/wiki/"
+''' 
+While parsing links on the site, we will need to handle both 
+absolute (http://site/subfolder) and relative (/subfolder/) links. 
+
+And when we convert the path into a filename, it will be handy to 
+know the length of the url as a string, in order to quickly remove
+it from the file path
+'''
+URL_LENGTH = len(URL)
+''' grab the various parts of the url '''
+PARSED = urlparse(URL)
+DOMAIN = f"{PARSED.scheme}://{PARSED.netloc}"
+PATH = PARSED.path
+PATH_LENGTH = len(PATH) if len(PATH.replace('/', '')) > 0 else 0
 
 
 
@@ -12,22 +28,63 @@ def download(url):
     print(f"fetching '{url}'")
     try: 
       r = requests.get(url)
-      page_links = get_links(r.text)
+      page_links = get_links(r.text, url)
       for link in page_links: 
         print(f"{url} links to {link}")
     except requests.ConnectionError as ce:
       print(f"could not fetch '{url}'", ce)
 
     
-'''
-Here's the new method that takes the HTML and finds all the links
-'''
-def get_links(html):
+def get_links(html, baseurl):
     soup = BeautifulSoup(html, 'html.parser')
     links = []
     for link in soup.find_all('a'):
       href = link.get('href')
-      links.append(href)
+      '''
+      Here's a little safety check: there may be links where the href never gets set. 
+      Sadly your scraper may have to work around poorly formed HTML like this.
+      '''
+      if href:
+          if href.lower().startswith("http"):
+              '''
+              The link starts with the full website. Is it the one we want? 
+              '''
+              if href.startswith(URL):
+                  ''' this is an absolute url that lives under our target site '''
+                  links.append(href)
+          elif href.lower().startswith(f"//{PARSED.netloc}"):
+              ''' urls can start with the double slash instead of specifying "https" or "http" '''
+              href = f"{PARSED.scheme}:{href}"
+              if href.startswith(URL):
+                  ''' this is an absolute url that lives under our target site '''
+                  links.append(href)
+          elif href.startswith('#'): 
+              ''' skip it, it's just another part of the same html file '''
+              pass
+          elif '/' == href[0]:
+              if PATH_LENGTH == 0: 
+                  ''' our target is the root domain of the site '''
+                  links.append(DOMAIN + href)  
+              elif href[0: PATH_LENGTH] == PATH:
+                  ''' 
+                  our target is a folder somewhere below the root domain of the site, 
+                  but the path here matches it
+                  '''
+                  links.append(DOMAIN + href)
+          else:
+              ''' 
+              this link is relative to the url that we were just on 
+              '''
+              if baseurl.endswith('/'): 
+                  links.append(baseurl + href)
+              elif baseurl.endswith('.html'): 
+                  last_slash = baseurl.rfind('/')
+                  links.append(f"{baseurl[0:last_slash]}/{href}")
+                  print(f"transformed {href} and {baseurl} into {baseurl[0:last_slash]}/{href}")
+              else: 
+                  links.append(f"{baseurl}/{href}")
+                  
+      
     return links
 
 
